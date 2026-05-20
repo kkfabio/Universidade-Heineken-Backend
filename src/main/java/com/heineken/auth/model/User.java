66fc4469a7462b740fd8d781c1c7337b3a9a8b30
Fábio Nunes <fabionascfilho@gmail.com>
2026-05-20 16:39:02 -0300

    feat: Commit inicial para o desenvolvimento do projeto

---
+++ src/main/java/com/heineken/auth/model/User.java
@@ -0,0 +1,32 @@
+package com.heineken.auth.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private String role = "USER";
+}
