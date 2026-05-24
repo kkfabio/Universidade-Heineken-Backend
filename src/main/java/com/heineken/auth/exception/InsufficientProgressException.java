76fd6b95a3f7fe50eaba26a43b1bb1d5afc0574a
moouraajv <joaovictormbpdev@hotmail.com>
2026-05-24 20:17:05 -0300

    feat: implementar segurança JWT, exceções de domínio e checkpoint (tasks 7-9)

---
+++ src/main/java/com/heineken/auth/exception/InsufficientProgressException.java
@@ -0,0 +1,7 @@
+package com.heineken.auth.exception;
+
+public class InsufficientProgressException extends RuntimeException {
+    public InsufficientProgressException(String message) {
+        super(message);
+    }
+}
